3ae2c7567aea82959f5fd9a8f4a266581709039c
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2023-02-07 15:57:26 +0100

    enemy collision detection

--- lab6-canvas/index.js
+++ lab6-canvas/index.js
@@ -22,6 +22,7 @@ class Circle {
 	}
 	draw(context) {
 		context.beginPath()
+		context.lineWidth = 0
 		context.arc(
 			this.xposition,
 			this.yposition,
@@ -30,11 +31,27 @@ class Circle {
 			Math.PI * 2,
 			false
 		)
+		context.strokeStyle = this.color
 		context.stroke()
+		context.fillStyle = this.color
+		context.fill()
 		context.closePath()
 	}
 	move() {
 		this.draw(context)
+
+		if (this.xposition + this.radius > windowWidth) {
+			this.dx = -this.dx
+		}
+		if (this.xposition - this.radius < 0) {
+			this.dx = -this.dx
+		}
+		if (this.yposition + this.radius > windowHeight) {
+			this.dy = -this.dy
+		}
+		if (this.yposition - this.radius < 0) {
+			this.dy = -this.dy
+		}
 
 		this.xposition += this.dx
 		this.yposition += this.dy
@@ -56,26 +73,65 @@ let drawCircle = (circle) => {
 
 let circlesOnBoard = []
 
-// let createXCircles = () => {
-// 	let amount = Math.random() * 20 - 1
+let createXCircles = () => {
+	let amount = Math.random() * 20 + 2
 
-// 	for (let i = 1; i < amount; i++) {
-// 		// xposition, yposition, radius, color, speed, dx, dy
-// 		let { xpos, ypos, radius, color } = decorateCircle()
-// 		let newCircle = new Circle(xpos, ypos, radius, color, 1)
-// 		circlesOnBoard.push(newCircle)
-// 		newCircle.draw(context)
-// 	}
-// }
-// createXCircles()
+	for (let i = 1; i < amount; i++) {
+		// xposition, yposition, radius, color, speed, dx, dy
+		let { xpos, ypos, radius, color } = decorateCircle()
+		let newCircle = new Circle(xpos, ypos, radius, color, 5)
+		circlesOnBoard.push(newCircle)
+		newCircle.draw(context)
+	}
+}
+createXCircles()
 
-// let moveCircles = () => {
-// 	requestAnimationFrame(moveCircles)
-// 	circlesOnBoard.forEach((circle) => circle.move())
-// }
-// moveCircles()
-let { xpos, ypos, radius, color } = decorateCircle()
-console.log(xpos, ypos, radius, color)
-let newCircle = new Circle(xpos, ypos, radius, color, 1)
-console.log(newCircle)
-newCircle.draw(context)
+let calculateDistance = (xpos1, xpos2, ypos1, ypos2) => {
+	let distance = Math.sqrt(
+		Math.pow(xpos2 - xpos1, 2) + Math.pow(ypos2 - ypos1, 2)
+	)
+	return distance
+}
+
+let handleCircleCollision = (circ1, circ2) => {
+	if (
+		calculateDistance(
+			circ1.xposition,
+			circ2.xposition,
+			circ1.yposition,
+			circ2.yposition
+		) <
+		circ2.radius + circ1.radius
+	) {
+		let absorbsionPower
+		let eatenCircle
+		if (circ1.radius > circ2.radius) {
+			eatenCircle = circ2
+		} else {
+			eatenCircle = circ1
+		}
+		absorbsionPower = eatenCircle.radius * 0.0005
+		if (eatenCircle.radius - absorbsionPower <= 10) {
+			circlesOnBoard.pop(eatenCircle)
+		}
+		eatenCircle.strokeStyle = 'red'
+		eatenCircle.radius -= absorbsionPower
+		eatenCircle === circ1
+			? (circ2.radius += absorbsionPower)
+			: (circ1.radius += absorbsionPower)
+	} else {
+		circ2.strokeStyle = circ2.color
+		circ1.strokeStyle = circ1.color
+	}
+}
+
+let moveCircles = () => {
+	requestAnimationFrame(moveCircles)
+	context.clearRect(0, 0, windowWidth, windowHeight)
+	circlesOnBoard.forEach((circle) => {
+		circle.move()
+		let enemyCircles = circlesOnBoard.filter((other) => other !== circle)
+		enemyCircles.forEach((enemy) => handleCircleCollision(circle, enemy))
+	})
+}
+moveCircles()
