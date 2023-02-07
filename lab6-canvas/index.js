73ff42d02ba3d7a1c72b01e98216ff3f25862a05
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2023-02-07 12:59:58 +0100

    circle decoration corrected

--- lab6-canvas/index.js
+++ lab6-canvas/index.js
@@ -5,18 +5,20 @@ let windowHeight = window.innerHeight
 let windowWidth = window.innerWidth
 
 canvas.width = windowWidth
-canvas.hight = windowHeight
+canvas.height = windowHeight
 
+let idCounter = 0
 class Circle {
-	constructor(id, xposition, yposition, radius, color, speed, dx, dy) {
-		this.id = id
+	constructor(xposition, yposition, radius, color, speed) {
+		this.id = idCounter++
 		this.xposition = xposition
 		this.yposition = yposition
 		this.radius = radius
 		this.color = color
 		this.speed = speed
-		this.dx = dx
-		this.dy = dy
+		this.dx = 1 * this.speed
+		this.dy = 1 * this.speed
+		idCounter++
 	}
 	draw(context) {
 		context.beginPath()
@@ -40,12 +42,40 @@ class Circle {
 }
 
 let decorateCircle = () => {
-	let xpos = Math.random() * windowWidth
-	let ypos = Math.random() * windowHeight
-	let radius = (Math.random() * windowWidth) / 5
-	let color = Math.floor(Math.random() * 16777215).toString(16)
+	let radius = Math.random() * (windowWidth / 10) + 20
+
+	let xpos = Math.random() * (windowWidth - 2 * radius - radius) + (radius + 1)
+	let ypos = Math.random() * (windowHeight - 2 * radius - radius) + (radius + 1)
+	let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+	return { xpos, ypos, radius, color }
 }
 
 let drawCircle = (circle) => {
 	circle.draw(context)
 }
+
+let circlesOnBoard = []
+
+// let createXCircles = () => {
+// 	let amount = Math.random() * 20 - 1
+
+// 	for (let i = 1; i < amount; i++) {
+// 		// xposition, yposition, radius, color, speed, dx, dy
+// 		let { xpos, ypos, radius, color } = decorateCircle()
+// 		let newCircle = new Circle(xpos, ypos, radius, color, 1)
+// 		circlesOnBoard.push(newCircle)
+// 		newCircle.draw(context)
+// 	}
+// }
+// createXCircles()
+
+// let moveCircles = () => {
+// 	requestAnimationFrame(moveCircles)
+// 	circlesOnBoard.forEach((circle) => circle.move())
+// }
+// moveCircles()
+let { xpos, ypos, radius, color } = decorateCircle()
+console.log(xpos, ypos, radius, color)
+let newCircle = new Circle(xpos, ypos, radius, color, 1)
+console.log(newCircle)
+newCircle.draw(context)
