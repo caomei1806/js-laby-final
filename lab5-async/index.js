b8763a52a94c34ddbc2cfff28eb80aabad752b1b
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2023-02-15 21:54:56 +0100

    adding data fetch && time measurement

--- lab5-async/index.js
+++ lab5-async/index.js
@@ -11,11 +11,31 @@ const asyncAdd = async (a, b) => {
 		}, 100)
 	})
 }
+const fetchArguments = (args) => {
+	console.log(args)
 
-const fetchArguments = (numbers) => {
-	const numbers = [...numbers]
+	const numbers = [...args]
 	numbers.forEach(async (number) => {
 		const newSum = await asyncAdd(sum, number).then(() => asyncInvokeCounter++)
 		sum = newSum
 	})
 }
+const fetchRandomData = async (dataFile) => {
+	const data = await fetch(`./${dataFile}`)
+	const result = await data.json()
+	console.log(result)
+	return result
+}
+
+const performDataCalculation = async () => {
+	const randomData = fetchRandomData('data.json')
+		.then((data) => data)
+		.then((data) => {
+			let start = performance.now()
+			fetchArguments(data)
+			let end = performance.now()
+			let timestamp = end - start
+			console.log('Exec time: ' + timestamp)
+		})
+}
+performDataCalculation()
