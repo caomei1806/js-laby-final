ea909853e57f706c94f9f71fae20c82b622f4132
Karolina Grochal <46192501+caomei1806@users.noreply.github.com>
2023-02-17 14:19:47 +0100

    updating weather icons && correcting weather delete && setting up max amount of weather boxes && creating error message

--- lab7-pogoda/index.js
+++ lab7-pogoda/index.js
@@ -4,7 +4,6 @@ const api = {
 	key: '3e81eb49d3ead8997fd68eee40d8904c',
 	base: 'https://api.openweathermap.org/data/2.5/',
 }
-
 let weather = []
 let searched = ''
 
@@ -25,10 +24,99 @@ const search = async () => {
 			})
 		weather.push(currentWeather)
 		addCityWeather(currentWeather)
-		console.log(weather)
 	} catch (err) {
 		console.log('Error: ' + err)
+	} finally {
+		query.value = ''
 	}
+}
+
+const buildWeatherDOM = (weatherDetails) => {
+	const cityWeatherBox = document.createElement('div')
+	cityWeatherBox.classList.add('weather-city')
+
+	const deleteButton = document.createElement('button')
+	deleteButton.classList.add('btn', 'btn-delete')
+	deleteButton.textContent = 'X'
+
+	cityWeatherBox.appendChild(deleteButton)
+
+	const cityName = document.createElement('p')
+	cityName.textContent = weatherDetails.city
+	cityName.classList.add('weather-city-name')
+
+	cityWeatherBox.appendChild(cityName)
+
+	Object.keys(weatherDetails).forEach(async (detail) => {
+		const simpleWeather = weatherDetails['simpleWeather']
+		const weatherId = weatherDetails['weatherId']
+
+		if (
+			detail !== 'city' &&
+			detail !== 'simpleWeather' &&
+			detail !== 'weatherId'
+		) {
+			const detailBox = document.createElement('div')
+			detailBox.classList.add(`weather-city-${detail}`)
+			const detailIcon = await matchWeatherDetailIcon(detail)
+			detailBox.textContent = `${detail} `
+			if (detailIcon) {
+				detailBox.innerHTML += `${detailIcon} <br />`
+			}
+			if (detail !== 'wind') {
+				const detailNo = document.createElement('span')
+				if (detail !== 'weather') {
+					let unit = ''
+					switch (detail) {
+						case 'temp':
+							unit = '°C'
+							break
+						case 'feels_like':
+							unit = '°C'
+							break
+						case 'humidity':
+							unit = '%'
+							break
+						case 'presure':
+							unit = 'hPa'
+							break
+						case 'presure':
+							unit = 'hPa'
+							break
+						default:
+							break
+					}
+					detailNo.textContent = `${weatherDetails[detail]}${unit}`
+				} else {
+					const icon = await matchWeatherIcon(simpleWeather, weatherId)
+					if (icon) {
+						detailNo.innerHTML = `${icon} \n ${weatherDetails[detail]}`
+					}
+				}
+				detailNo.classList.add(`weather-city-${detail}No`)
+
+				detailBox.appendChild(detailNo)
+			} else {
+				detailBox.textContent = `${detail} `
+				detailBox.innerHTML += detailIcon
+
+				const windSpeed = document.createElement('div')
+				windSpeed.classList.add(`weather-city-wind`)
+				windSpeed.textContent = `Speed: ${weatherDetails[detail].speed}km/h`
+
+				const windDeg = document.createElement('div')
+				windDeg.classList.add(`weather-city-wind`)
+				windDeg.innerHTML = `Deg: ${weatherDetails[detail].deg}°`
+
+				detailBox.appendChild(windSpeed)
+				detailBox.appendChild(windDeg)
+			}
+
+			cityWeatherBox.appendChild(detailBox)
+		}
+	})
+
+	weatherBox.appendChild(cityWeatherBox)
 }
 
 const addCityWeather = (currentCityWeather) => {
@@ -48,60 +136,29 @@ const addCityWeather = (currentCityWeather) => {
 		country,
 		sunrise,
 		sunset,
-		description,
+		weather: description,
+		wind,
+		simpleWeather,
+		weatherId,
 	}
 
-	const cityWeatherBox = document.createElement('div')
-	cityWeatherBox.classList.add('weather-city')
+	const isAbleToAddNewElement = saveLocal(weatherDetails)
+	if (isAbleToAddNewElement) buildWeatherDOM(weatherDetails)
+}
 
-	const cityName = document.createElement('p')
-	cityName.textContent = weatherDetails.city
-	cityName.classList.add('weather-city-name')
+const matchWeatherDetailIcon = async (weatherDetail) => {
+	try {
+		const res = await fetch('./titleIcons.json')
+		const titleWeatherIcons = await res.json()
 
-	cityWeatherBox.appendChild(cityName)
-
-	Object.keys(weatherDetails).forEach(async (detail) => {
-		if (detail !== 'city') {
-			const detailBox = document.createElement('div')
-			detailBox.classList.add(`weather-city-${detail}`)
-			detailBox.textContent = `${detail}: `
-
-			const detailNo = document.createElement('span')
-			if (detail !== 'description')
-				detailNo.textContent = weatherDetails[detail]
-			else {
-				console.log(simpleWeather)
-				const icon = await matchWeatherIcon(simpleWeather, weatherId)
-				if (icon) {
-					detailNo.innerHTML = `${icon} \n ${weatherDetails[detail]}`
-				}
-			}
-			detailNo.classList.add(`weather-city-${detail}No`)
-
-			detailBox.appendChild(detailNo)
-
-			cityWeatherBox.appendChild(detailBox)
-		}
-	})
-
-	const windBox = document.createElement('div')
-	windBox.classList.add(`weather-city-wind`)
-	windBox.textContent = 'Wind: '
-
-	const windSpeed = document.createElement('div')
-	windSpeed.classList.add(`weather-city-wind`)
-	windSpeed.textContent = `Speed: ${speed}`
-
-	const windDeg = document.createElement('div')
-	windDeg.classList.add(`weather-city-wind`)
-	windDeg.textContent = `Deg: ${deg}`
-
-	windBox.appendChild(windSpeed)
-	windBox.appendChild(windDeg)
-
-	cityWeatherBox.appendChild(windBox)
-
-	weatherBox.appendChild(cityWeatherBox)
+		const iconByName = titleWeatherIcons.find(
+			(weather) => weather.name === weatherDetail
+		)
+		if (iconByName) return iconByName.icon
+		else return ''
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 const matchWeatherIcon = async (weatherDesc, weatherId = 0) => {
@@ -123,6 +180,98 @@ const matchWeatherIcon = async (weatherDesc, weatherId = 0) => {
 		console.log(err)
 	}
 }
+
+const saveLocal = (weather) => {
+	let weatherList
+	if (localStorage.getItem('weatherList') == null) {
+		weatherList = []
+	} else {
+		weatherList = JSON.parse(localStorage.getItem('weatherList'))
+	}
+	if (weatherList.length < 10) {
+		weatherList.push(weather)
+		localStorage.setItem('weatherList', JSON.stringify(weatherList))
+		return 1
+	} else {
+		const errorMessage = document.createElement('p')
+		errorMessage.classList.add('error')
+		errorMessage.textContent =
+			'You could only search up to 10 weather elements! Please delete weather element to be able to get a new one.'
+
+		const deleteButton = document.createElement('button')
+		deleteButton.classList.add('btn', 'btn-delete', 'btn-light')
+		deleteButton.textContent = 'X'
+
+		errorMessage.appendChild(deleteButton)
+
+		query.parentElement.appendChild(errorMessage)
+		return 0
+	}
+}
+
+const showWeather = () => {
+	weatherBox.replaceChildren()
+
+	let weatherList
+	if (localStorage.getItem('weatherList') == null) {
+		weatherList = []
+	} else {
+		weatherList = JSON.parse(localStorage.getItem('weatherList'))
+	}
+
+	weatherList.forEach((weather) => {
+		console.log(weather)
+		buildWeatherDOM(weather)
+	})
+}
+document.addEventListener('DOMContentLoaded', showWeather)
+
+const removeWeather = (weather) => {
+	let weatherList
+	if (localStorage.getItem('weatherList') == null) {
+		weatherList = []
+	} else {
+		weatherList = JSON.parse(localStorage.getItem('weatherList'))
+	}
+	//console.log(weather.children[1].textContent)
+	const weatherToDelete = weatherList.find(
+		(currentWeather) => currentWeather.city === weather.children[1].textContent
+	)
+	//weatherList.forEach((weather) => console.log(weather.city))
+	const newWeatherList = weatherList.filter(
+		(weather) => weather !== weatherToDelete
+	)
+	localStorage.setItem('weatherList', JSON.stringify(newWeatherList))
+}
+
+const deleteWeather = (event) => {
+	const item = event.target
+	if (item.classList[1] == 'btn-delete') {
+		const weatherToDelete = item.parentElement
+		console.log(weatherToDelete)
+		weatherToDelete.classList.add('deleted')
+		removeWeather(weatherToDelete)
+		weatherToDelete.addEventListener('transitionend', function () {
+			weatherToDelete.remove()
+		})
+		showWeather()
+	}
+}
+const deleteElement = (event) => {
+	const item = event.target
+	if (item.classList[1] == 'btn-delete') {
+		console.log(item.classList)
+
+		const elementToDelete = item.parentElement
+		elementToDelete.classList.add('deleted')
+
+		elementToDelete.addEventListener('transitionend', function () {
+			elementToDelete.remove()
+		})
+	}
+}
+weatherBox.addEventListener('click', deleteWeather)
+query.parentElement.addEventListener('click', deleteElement)
 
 const dateBuilder = (today) => {
 	let date = String(new window.Date())
